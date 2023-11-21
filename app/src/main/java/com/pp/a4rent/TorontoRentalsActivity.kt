1b80f0741b6610dacac7b13d676e8324d604c66e
Pabitapun23 <pabita.pun23@gmail.com>
2023-11-21 13:02:27 -0500

    rental post list display and models created

--- app/src/main/java/com/pp/a4rent/TorontoRentalsActivity.kt
+++ app/src/main/java/com/pp/a4rent/TorontoRentalsActivity.kt
@@ -8,17 +8,34 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
-import com.pp.a4rent.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.pp.a4rent.adapters.TorontoRentalsAdapter
+import com.pp.a4rent.databinding.ActivityTorontoRentalsBinding
+import com.pp.a4rent.models.Owner
+import com.pp.a4rent.models.PropertyRental
 
-class MainActivity : AppCompatActivity() {
+class TorontoRentalsActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private val TAG : String = "MainActivity"
+    private lateinit var binding: ActivityTorontoRentalsBinding
+    private val TAG : String = "TorontoRentalsActivity"
+
+    // Made the adapter global
+    private lateinit var adapter: TorontoRentalsAdapter
+
+    // made this mutable because we know that later on, we have to add fruits to it
+    private var rentalDatasource:MutableList<PropertyRental> = mutableListOf<PropertyRental>(
+        PropertyRental("condo", Owner("Peter", "peter@gmail.com", 123), 12, 2,3, "cool condo", "abc", 2500.0, true, "peter"),
+        PropertyRental("house", Owner("Amy", "amy@gmail.com", 123), 12, 2,3, "cool condo","abc", 2500.0, true, "amy"),
+        PropertyRental("apartment", Owner("Alex", "alex@gmail.com", 123), 12, 2,3, "cool condo", "abc", 2500.0, true, "alex"),
+        PropertyRental("basement", Owner("Jane", "jane@gmail.com", 123), 12, 2,3, "cool condo", "abc", 2500.0, true, "jane"),
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_toronto_rentals)
 
-        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        this.binding = ActivityTorontoRentalsBinding.inflate(layoutInflater)
         setContentView(this.binding.root)
 
         // display menu bar on the screen
@@ -37,19 +54,43 @@ class MainActivity : AppCompatActivity() {
             binding.menuToolbar.overflowIcon = wrappedDrawable
         }
 
-        binding.btnSearchToronto.setOnClickListener {
-            this.goToTorontoRentals()
-        }
+        // setup adapter
+        // Update adapter to accept a list of rentals
+        this.adapter = TorontoRentalsAdapter(
+            rentalDatasource,
+            {pos -> rowClicked(pos) },
+            {pos -> favButtonClicked(pos)}
+
+        )
+
+        // setup rv
+        binding.rvItems.adapter = adapter
+        binding.rvItems.layoutManager = LinearLayoutManager(this)
+        binding.rvItems.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                LinearLayoutManager.VERTICAL
+            )
+        )
+
     }
 
-    // Function to navigate users to Toronto Rentals page
-    // Shows list of rentals in Toronto area
-    fun goToTorontoRentals() {
+    // rv:  Row click handler
+    fun rowClicked(position:Int){
 
-        // navigate to 2nd screen
-        val torontoRentalsIntent = Intent(this@MainActivity, TorontoRentalsActivity::class.java)
-        startActivity(torontoRentalsIntent)
+        var selectedRental:PropertyRental = rentalDatasource.get(position)
+        // snackbar
+        val snackbar = Snackbar.make(binding.root, "${selectedRental.toString()}", Snackbar.LENGTH_LONG)
+        snackbar.show()
     }
+
+    // rv: Favorite button click handler
+    fun favButtonClicked(position:Int) {
+        val snackbar = Snackbar.make(binding.root, "Favorite ${position}", Snackbar.LENGTH_LONG)
+        snackbar.show()
+    }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.home_menu_options, menu)
@@ -64,7 +105,7 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "onOptionsItemSelected: Post Rental option is selected")
 
                 // navigate to 2nd screen
-                val sidebarIntent = Intent(this@MainActivity, ProfileActivity::class.java)
+                val sidebarIntent = Intent(this@TorontoRentalsActivity, ProfileActivity::class.java)
                 startActivity(sidebarIntent)
 
                 return true
